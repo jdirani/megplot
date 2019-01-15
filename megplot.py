7fc0e5b3e7203baf70751557a4abb82e0374c812
Julien Dirani <juliendirani@gmail.com>
2019-01-15 13:01:05 +0400

    check sign clusters
    check whether there are significant clusters before continuing (line50) to avoid error.

--- megplot.py
+++ megplot.py
@@ -46,42 +46,43 @@ def plot(res, ds, out_dir, match, subjects_dir, surf=None, hemi=None):
 
     mask_sign_clusters = np.where(res.clusters['p'] <= 0.05, True, False)
     sign_clusters = res.clusters[mask_sign_clusters]
+    
+    if sign_clusters.n_cases != None: #check for significant clusters
+        for i in range(sign_clusters.n_cases):
+            cluster_nb = i+1
+            cluster = sign_clusters[i]['cluster']
+            tstart = sign_clusters[i]['tstart']
+            tstop = sign_clusters[i]['tstop']
+            effect = sign_clusters[i]['effect']
 
-    for i in range(sign_clusters.n_cases):
-        cluster_nb = i+1
-        cluster = sign_clusters[i]['cluster']
-        tstart = sign_clusters[i]['tstart']
-        tstop = sign_clusters[i]['tstop']
-        effect = sign_clusters[i]['effect']
+            effect = effect.replace(' x ', '%') # Changing format of string for interaction effects.
 
-        effect = effect.replace(' x ', '%') # Changing format of string for interaction effects.
-
-        # save significant cluster as a temporary label for plotting.
-        ds['stc'] = src
-        label = eelbrain.labels_from_clusters(cluster)
-        label[0].name = 'TempLabel-%s'%hemi
-        mne.write_labels_to_annot(label,subject='fsaverage', parc='workspace' ,subjects_dir=subjects_dir, overwrite=True)
-        src.source.set_parc('workspace')
-        src_region = src.sub(source='TempLabel-%s'%hemi)
-        ds['stc'] = src_region
-        timecourse = src_region.mean('source')
+            # save significant cluster as a temporary label for plotting.
+            ds['stc'] = src
+            label = eelbrain.labels_from_clusters(cluster)
+            label[0].name = 'TempLabel-%s'%hemi
+            mne.write_labels_to_annot(label,subject='fsaverage', parc='workspace' ,subjects_dir=subjects_dir, overwrite=True)
+            src.source.set_parc('workspace')
+            src_region = src.sub(source='TempLabel-%s'%hemi)
+            ds['stc'] = src_region
+            timecourse = src_region.mean('source')
 
 
-        #Plot
-            # 1)Timecourse
-        activation = eelbrain.plot.UTSStat(timecourse, effect, ds=ds, title = 'Effect = %s, cluster #%s'%(effect, cluster_nb))
-        activation.add_vspan(xmin=tstart, xmax=tstop, color='lightgrey', zorder=-50)
-        activation.save(out_dir+'Cluster #%s, Effect = %s - Timecourse.png'%(cluster_nb, effect))
-        activation.close()
-            # 2) Brain
-        brain = eelbrain.plot.brain.cluster(cluster.mean(time=(tstart,tstop)), subjects_dir=subjects_dir, surf=surf, colorbar=True)
-        brain.save_image(out_dir+'Cluster #%s, Effect = %s - Brain.png'%(cluster_nb, effect))
-        brain.close()
-            # 3) Bars
-        ds['average_source_activation'] = timecourse.mean(time=(tstart,tstop))
-        bar = eelbrain.plot.Barplot(ds['average_source_activation'], X=effect, ds=ds, match=match)
-        bar.save(out_dir+'Cluster #%s, Effect = %s - Bars.png'%(cluster_nb, effect))
-        bar.close()
+            #Plot
+                # 1)Timecourse
+            activation = eelbrain.plot.UTSStat(timecourse, effect, ds=ds, title = 'Effect = %s, cluster #%s'%(effect, cluster_nb))
+            activation.add_vspan(xmin=tstart, xmax=tstop, color='lightgrey', zorder=-50)
+            activation.save(out_dir+'Cluster #%s, Effect = %s - Timecourse.png'%(cluster_nb, effect))
+            activation.close()
+                # 2) Brain
+            brain = eelbrain.plot.brain.cluster(cluster.mean(time=(tstart,tstop)), subjects_dir=subjects_dir, surf=surf, colorbar=True)
+            brain.save_image(out_dir+'Cluster #%s, Effect = %s - Brain.png'%(cluster_nb, effect))
+            brain.close()
+                # 3) Bars
+            ds['average_source_activation'] = timecourse.mean(time=(tstart,tstop))
+            bar = eelbrain.plot.Barplot(ds['average_source_activation'], X=effect, ds=ds, match=match)
+            bar.save(out_dir+'Cluster #%s, Effect = %s - Bars.png'%(cluster_nb, effect))
+            bar.close()
 
 
     ds['stc'] = src # resets ds['stc'] before exiting the loop. Otherwise, returns an errror if make_report is run again.
